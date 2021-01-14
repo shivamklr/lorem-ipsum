73bc4144e7843df8f63464769665e841aec192a9
Shivam Kalra <shivamkalra98@gmail.com>
2021-01-14 17:06:06 +0530

    Add basic css

--- src/App.js
+++ src/App.js
@@ -1,49 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import data from "./data";
-import "./App.css";
+
 function App() {
     const [counter, setcounter] = useState(0);
     const [loremtext, setloremtext] = useState([]);
-    // const loremGenerator = (counter) => {
-    //     let temp = "";
-    //     if (counter < 0) {
-    //         temp += data[0];
-    //     }
-    //     for (var i = 0; i < counter && i < data.length; i++) {
-    //         temp += data[i];
-    //         temp += "\n";
-    //     }
-    //     setloremtext(temp);
-    // };
+
     const submitHandler = (e) => {
         e.preventDefault();
-        let temptext = [];
-        if (counter < 0) {
-            temptext[0] = <p>{data[0]}</p>;
-        } else {
-            temptext = data.map((text, index) => {
-                if (index < counter) return <p key={index}>{text}</p>;
-            });
+        let amount = parseInt(counter);
+        if (amount <= 0) {
+            amount = 1;
         }
-
-        setloremtext(temptext);
+        if (amount > data.length) {
+            amount = data.length;
+        }
+        setloremtext(data.slice(0, amount));
     };
-    // useEffect(() => {
-    //     loremGenerator(counter);
-    // }, [counter]);
     return (
-        <>
-            <form className="Lorem-form" onSubmit={submitHandler}>
-                Paragraphs:
-                <input
-                    type="number"
-                    value={counter}
-                    onChange={(e) => setcounter(e.target.value)}
-                />
-                <button type="submit">Generate</button>
-            </form>
-            {loremtext}
-        </>
+        <div className="App">
+            <section className="lorem-section">
+                <h3>Generate lorem ipsum text right here</h3>
+                <form className="lorem-form" onSubmit={submitHandler}>
+                    <label htmlFor="count">Paragraphs:</label>
+                    <input
+                        id="count"
+                        type="number"
+                        value={counter}
+                        onChange={(e) => setcounter(e.target.value)}
+                    />
+                    <button type="submit">Generate</button>
+                </form>
+                {loremtext.map((text, index) => {
+                    return (
+                        <p classname="lorem-para" key={index}>
+                            {text}
+                        </p>
+                    );
+                })}
+            </section>
+        </div>
     );
 }
 
