08f735ecb3125eb599a323163adf0be162b9a5fa
Shivam Kalra <shivamkalra98@gmail.com>
2021-01-14 14:32:25 +0530

    generate text on submit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import "./App.css";
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    const [counter, setcounter] = useState(0);
+    const [loremtext, setloremtext] = useState([]);
+    // const loremGenerator = (counter) => {
+    //     let temp = "";
+    //     if (counter < 0) {
+    //         temp += data[0];
+    //     }
+    //     for (var i = 0; i < counter && i < data.length; i++) {
+    //         temp += data[i];
+    //         temp += "\n";
+    //     }
+    //     setloremtext(temp);
+    // };
+    const submitHandler = (e) => {
+        e.preventDefault();
+        let temptext = [];
+        if (counter < 0) {
+            temptext[0] = <p>{data[0]}</p>;
+        } else {
+            temptext = data.map((text, index) => {
+                if (index < counter) return <p key={index}>{text}</p>;
+            });
+        }
+
+        setloremtext(temptext);
+    };
+    // useEffect(() => {
+    //     loremGenerator(counter);
+    // }, [counter]);
+    return (
+        <>
+            <form className="Lorem-form" onSubmit={submitHandler}>
+                Paragraphs:
+                <input
+                    type="number"
+                    value={counter}
+                    onChange={(e) => setcounter(e.target.value)}
+                />
+                <button type="submit">Generate</button>
+            </form>
+            {loremtext}
+        </>
+    );
 }
 
 export default App;
